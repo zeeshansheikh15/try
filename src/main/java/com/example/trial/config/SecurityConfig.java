42fb76c29cddb4c922a69981ca23f0b4065ce75d
zeeshan sheikh <32232605+zeeshansheikh15@users.noreply.github.com>
2020-01-09 16:36:45 -0600

    security

--- src/main/java/com/example/trial/config/SecurityConfig.java
+++ src/main/java/com/example/trial/config/SecurityConfig.java
@@ -1,34 +1,38 @@
 package com.example.trial.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	 @Override
-	   protected void configure(HttpSecurity http) throws Exception {
-	      http
-	         .authorizeRequests()
-	            .antMatchers("/**").permitAll()
-	            .anyRequest().authenticated()
-	            .and()
-	         .formLogin()
-	            .loginPage("/demo/add")
-	            .permitAll()
-	            .and()
-	            .logout()
-	            .permitAll();
-	   }
-	   @Autowired
-	   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-	      auth
-	         .inMemoryAuthentication()
-	         .withUser("user").password("user").roles("USER");
-	   }
+	/*
+	 * @Override protected void configure(HttpSecurity http) throws Exception { http
+	 * .authorizeRequests() .antMatchers("/**").permitAll()
+	 * .anyRequest().authenticated() .and() .formLogin() .loginPage("/demo/add")
+	 * .permitAll() .and() .logout() .permitAll(); }
+	*/
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("zee").password("zee").roles("ADMIN")
+		.and()
+		.withUser("user").password("user").roles("user");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+		
+	}
+	
 	}
